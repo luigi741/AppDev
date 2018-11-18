31e39f4094ab33f4d2403f53a1a12d6c5c6a7bcb
Luis Castro <luigi_789@hotmail.com>
2018-11-18 12:03:34 -0500

    Configuring SQLite

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,33 @@
 // Created an SQLite DB
 var db = window.sqlitePlugin.openDatabase({name: 'mySQLite.db', location: 'default'});
 
+db.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS invList (id integer primary key, title text, desc text)', [],
+    function(tx, result) {
+        alert('Table created successfully');
+    },
+    function(error) {
+        alert('Error occurred while creating table');
+    });
+});
+
+function insertData() {
+    var name = document.getElementById('dName').innerHTML;
+    var location = document.getElementById('dLocation').innerHTML;
+
+    db.transaction(function(transaction) {
+        var executeQuery = 'INSERT INTO invList (name, location) VALUE (?, ?)';
+        transaction.executeSql(executeQuery, [name, location],
+            function() {
+                alert('Inserted');
+            },
+            function(error) {
+                alert('Error occurred');
+            }
+        );
+    });
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -42,16 +69,32 @@ function dialogShow() {
     item.location = document.getElementById('location').value;
 
     var list1 = document.createElement('P');
+    list1.setAttribute('id', 'dName');
     list1.innerHTML = item.name;
 
     var list2 = document.createElement('P');
+    list2.setAttribute('id', 'dLocation');
     list2.innerHTML = item.location;
 
-    document.getElementById('dialogList').appendChild(list2);
     document.getElementById('dialogList').appendChild(list1);
+    document.getElementById('dialogList').appendChild(list2);
     document.getElementById('dialog-1').show();
 
+    clearForm();
+}
 
+function clearForm() {
+    document.getElementById('listName').value = '';
+    document.getElementById('location').value = '';
+
+}
+
+function clearDialog() {
+    var name = document.getElementById('dName');
+    name.parentNode.removeChild(name);
+
+    var location = document.getElementById('dLocation');
+    location.parentNode.removeChild(location);
 }
 
 function dialogClose() {
@@ -67,6 +110,5 @@ function homePage() {
     var home = document.getElementById('canvas');
     canvas.style = 'visibility: hidden';
 }
-
 
 app.initialize();
